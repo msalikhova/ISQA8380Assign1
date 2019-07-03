3ece8a86f83d5302dc1ef383291bc25a6bc3d844
msalikhova <msalikhova@unomaha.edu>
2019-07-03 15:36:19 -0500

    Changes as per tutorial

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }},this is an about page</h1>
+    <p> This is a Malika's To Do list developed  in Vue.Js to learn Vue.Js </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
